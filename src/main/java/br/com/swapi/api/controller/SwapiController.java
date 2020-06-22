9f91e855fabb4d289951b10a683e57d08ca7efd8
Dev <vicentcdb@gmail.com>
2020-06-22 14:13:13 -0300

    get all external api planets

--- src/main/java/br/com/swapi/api/controller/SwapiController.java
+++ src/main/java/br/com/swapi/api/controller/SwapiController.java
@@ -6,17 +6,16 @@ import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import br.com.swapi.api.model.Planeta;
 import br.com.swapi.api.service.SearchApiService;
@@ -35,6 +34,15 @@ public class SwapiController {
 		String dataTest = ".";
 		System.out.println(getNumPlanets());
 		return getNumPlanets().toString();
+	}
+	
+	@GetMapping(path = "/search/all/external")
+	public ArrayNode allPlanetsExternalApi() throws Exception {
+		
+		SearchApiService getPlanets_ = new SearchApiService();
+		ArrayNode numplanets = getPlanets_.getAllPlanetsExternal();
+				
+		return numplanets;
 	}
 
 	@GetMapping(path = "/search/{id}")
--- src/main/java/br/com/swapi/api/service/SearchApiService.java
+++ src/main/java/br/com/swapi/api/service/SearchApiService.java
@@ -3,10 +3,17 @@ package br.com.swapi.api.service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class SearchApiService {
 
@@ -48,6 +55,42 @@ public class SearchApiService {
 		}
 
 		return null;
+	}
+	
+	public ArrayNode getAllPlanetsExternal() throws Exception {
+		
+		JSONObject numPlanets = this.getBuilder("planets",null);
+		int numPlanetsConvert = Integer.valueOf(numPlanets.get("count").toString());
+		
+		//System.out.println("NUMERO DE PLANETAS###### " + numPlanets.get("count"));
+		
+		ArrayList<String> planetInfos = new ArrayList<String>();
+		ObjectMapper objMapper = new ObjectMapper();
+		
+		ArrayNode arrayNode = objMapper.createArrayNode();
+		
+		for(int i = 1; i <= 10; i++ ) {
+			
+			try {
+				
+				JSONObject numPlanetsById = this.getBuilder("planets",""+i+"");
+				
+				System.out.println("PLANETA>>>>>>>>>>>> " + numPlanetsById.toString());
+				
+				ObjectNode parentNode = objMapper.createObjectNode();
+				
+				JsonNode jsonNode = objMapper.readTree(numPlanetsById.toString());
+				
+				parentNode.set("planet" + numPlanetsById.get("name"), jsonNode);
+				
+				arrayNode.add(parentNode);
+				
+			} catch (Exception e) {	}			
+			
+				return arrayNode;
+		}
+		
+		return arrayNode;
 	}
 
 }
